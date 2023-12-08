56ab2b223e8839bdb0aa8e6f2247158ead459fed
Kevin <kevbao24@gmail.com>
2023-12-08 17:10:15 +0000

    Fetch API for weapon skins data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const resultContainer = document.getElementById('resultContainer');
+
+searchButton.addEventListener('click', () => {
+  const searchTerm = searchInput.value.trim().toLowerCase();
+
+  if (searchTerm !== '') {
+    const apiUrl = 'https://valorant-api.com/v1/weapons/skins';
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const matchingSkins = data.data.filter((skin) => {
+          return skin.displayName.toLowerCase().includes(searchTerm);
+        });
+      });
+  } else {
+    alert('Please enter a search term');
+  }
+});
